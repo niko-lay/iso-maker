97d2fcf417e33b6a68408524cf431709ad09b23e
Nikolay <djan.mail@gmail.com>
2021-08-26 10:02:15 +0000

    small fixes

--- runme.sh
+++ runme.sh
@@ -6,12 +6,12 @@ OUT_DIR=/iso-dir
 if [ "$#" -eq 0 ]; then
      echo "No packages list provided, default list will be used."
 fi
-CMDLINE="$@"
+CMDLINE="$*"
 PACKAGES_TO_INSTALL=${CMDLINE:-"lldpd ifenslave net-tools"}
 
 echo "Following packages will be added to iso: ${PACKAGES_TO_INSTALL}\n\n"
 
-if ! `mountpoint -q "${OUT_DIR}"` ; then
+if ! mountpoint -q "${OUT_DIR}" ; then
   echo "looks like ${OUT_DIR} is not mounted, don't want to store .iso inside container. Exiting..."
   exit 1
 fi
@@ -22,6 +22,6 @@ cd /debs
 apt-get download \
     $(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts \
       --no-breaks --no-replaces --no-enhances \
-      --no-pre-depends ${PACKAGES_TO_INSTALL} | grep "^\w")
+      --no-pre-depends "${PACKAGES_TO_INSTALL}" | grep "^\w")
 
 genisoimage -l -U -joliet-long -rock -o "${OUT_DIR}/to-install.iso" "/debs"
